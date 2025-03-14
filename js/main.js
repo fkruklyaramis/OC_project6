78a4df014f7a7d7c24d7e10b2de59c0c61287631
fkruklyaramis <francois.krukly@aramis.group>
2025-03-14 11:52:38 +0100

    fix modal

--- js/main.js
+++ js/main.js
@@ -30,17 +30,11 @@ function showMovieModal(movieData) {
     // Remplir les informations
     modal.querySelector('.modal-title').textContent = movieData.title;
     modal.querySelector('.modal-movie-img').src = movieData.image_url;
-    modal.querySelector('.modal-genre').textContent = movieData.genres.join(', ');
-    modal.querySelector('.modal-date').textContent = movieData.date_published;
-    modal.querySelector('.modal-rated').textContent = movieData.rated;
-    modal.querySelector('.modal-score').textContent = movieData.imdb_score;
+    modal.querySelector('.modal-date-genre').textContent = movieData.year + ' - ' + movieData.genres.join(', ');
+    modal.querySelector('.modal-pg-duration-countries').textContent = 'PG ' + ' - ' + movieData.duration + ' minutes (' + movieData.countries.join(' / ')+ ')';
+    modal.querySelector('.modal-imdb-score').textContent = 'IMDB score: ' + movieData.imdb_score + '/10';
+    modal.querySelector('.modal-income').textContent = 'Recettes au box-office: $' + movieData.worldwide_gross_income;
     modal.querySelector('.modal-directors').textContent = movieData.directors.join(', ');
     modal.querySelector('.modal-actors').textContent = movieData.actors.join(', ');
-    modal.querySelector('.modal-duration').textContent = movieData.duration;
-    modal.querySelector('.modal-countries').textContent = movieData.countries.join(', ');
-    modal.querySelector('.modal-income').textContent = 
-        movieData.worldwide_gross_income ? 
-        `${movieData.worldwide_gross_income.toLocaleString()} ${movieData.budget_currency}` : 
-        'Non disponible';
-    modal.querySelector('.modal-description').textContent = movieData.long_description || movieData.description;
+    modal.querySelector('.modal-description').textContent = movieData.long_description
 }
